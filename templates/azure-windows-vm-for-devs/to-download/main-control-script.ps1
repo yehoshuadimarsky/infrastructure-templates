c9932d00ef20401008caa2e7c8d9a88e0c1b9c67
Josh Dimarsky <24758845+yehoshuadimarsky@users.noreply.github.com>
2019-07-11 09:01:55 -0400

    typos

--- templates/azure-windows-vm-for-devs/to-download/main-control-script.ps1
+++ templates/azure-windows-vm-for-devs/to-download/main-control-script.ps1
@@ -14,12 +14,3 @@ function timestamp { Process{"$(Get-Date -Format o): $_"} };
 # Run script, with timestamp on each line of output and saved to specified log file
 .\setup_script.ps1 -userPasswordVal $userPassword | timestamp *> $logfile
 
-
-
-# Delete choco config file
-# $FileName = "choco-packages.config"
-# if (Test-Path $FileName) 
-# {
-#     Write-Host "Deleting the config file called $($FileName)"
-#     Remove-Item $FileName
-# }
