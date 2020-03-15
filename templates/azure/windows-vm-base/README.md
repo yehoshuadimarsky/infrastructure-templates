fe1f5756fe389cdbb76e3ea6503caef83fde8716
yehoshuadimarsky <24758845+yehoshuadimarsky@users.noreply.github.com>
2020-03-14 22:02:14 -0400

    renamed folder, readme

---
+++ templates/azure/windows-vm-base/README.md
@@ -0,0 +1,4 @@
+# Overview
+This creates a basic Windows VM in Azure, along with the associated resources required (networking, storage, etc.), all in a new dedicated Resource Group.
+
+To extend this template, copy it to a new folder, then modify it.
--- templates/azure/windows-vm-base/post-deploy.ps1
+++ templates/azure/windows-vm-base/post-deploy.ps1
@@ -137,9 +137,6 @@ Invoke-Command -Session $myPsSession -ScriptBlock {
         Set-ExecutionPolicy Bypass -Scope Process -Force
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     }
-     
-    # chrome
-    choco install googlechrome --yes
 }
 
 
